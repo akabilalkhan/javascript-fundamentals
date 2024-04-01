a866531790d2b8e4023e542e1a17d7d69317f11b
akabilalkhan <akakhanbilal@gmail.com>
2024-04-01 14:20:34 -0500

    Lesson Folder

---
+++ lessons/lesson2.js
@@ -0,0 +1,10 @@
+// Comment
+console.log("This is my first statement");
+
+/* Multiple
+comments */
+
+statement = "I am new to JavaScript";
+console.log(statement);
+
+console.log("I am learning stuff everyday");
