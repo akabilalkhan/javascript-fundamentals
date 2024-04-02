bef912350605bd0b7f6a05cdefee565727ca8f59
akabilalkhan <akakhanbilal@gmail.com>
2024-04-02 16:06:58 -0500

    numbers lab

---
+++ lessons/lesson4_numbers.js
@@ -0,0 +1,32 @@
+// Numbers Examples
+const value1 = 42;
+const value2 = 34.4;
+const value3 = -34.3;
+
+// 17 decimal point precision
+const value4 = 4.434343423425467;
+console.log(value4);
+
+// BigInt Values
+const value5 = 23333434344335533n;
+const value6 = 256n;
+console.log(value6);
+
+// Number Systems
+let ex_binary = 0b11010; // binary number
+let ex_hexadecimal = 0x1a; // Hexa
+let ex_octal = 0o32; // octal number
+console.log("0b11010 in Binary: " + ex_binary);
+
+// Exponential literals
+const aa = 0e-3;
+const bb = 4e2;
+const cc = 234e-21;
+console.log(cc);
+
+// Numeric seperators - using underscores for readability
+let dd = 9_563.25;
+let ee = 0b1010_0001; // binary
+let ff = 0xa0_b0_c0; // hexa
+console.log(dd);
+console.log(ff);
